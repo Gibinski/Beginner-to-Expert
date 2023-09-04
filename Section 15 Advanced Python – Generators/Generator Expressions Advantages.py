d345610611e0ecaaab0945c9edf809ed740d1660
Lyubomir Gibinski <l.gibinski@gmail.com>
2023-09-04 14:17:33 +0200

    Add gen 1

---
+++ Section 15 Advanced Python – Generators/Generator Expressions Advantages.py
@@ -0,0 +1,19 @@
+# ----------------- Generator Expressions Advantages
+
+#-*-*-*-*-*-*-*-* 1- Memory Efficient 
+#-*-*-*-*-*-*-*-* 2- Easy of Implementation
+#-*-*-*-*-*-*-*-* 3- Representing Infinite Stream
+#-*-*-*-*-*-*-*-* 4- Pipelining Generators
+
+
+
+# --**--**--**--**--** Example 5 ->>>> Easy of Implementation
+
+
+
+# --**--**--**--**--** Example 6 ->>>> Representing Infinite Stream
+
+
+
+# --**--**--**--**--** Example 7 ->>>> Pipelining Generators
+
