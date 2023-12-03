6848c636457c75cdcf4674ce43fde548aa4e620c
Gibinski <l.gibinski@gmail.com>
2023-12-03 13:12:56 -0800

    Add

---
+++ Section 18 Advanced Python Modules/4_dir_method.py
@@ -0,0 +1,9 @@
+# ---------------------------------------------------------------------
+# ---------------**## The Build-in dir() Method ##**---------------------
+from social_media.videos.user_videos import user_videos
+
+# print(dir(user_videos))
+
+print(user_videos.__name__)
+print(user_videos.__package__)
+print(user_videos.__file__)
