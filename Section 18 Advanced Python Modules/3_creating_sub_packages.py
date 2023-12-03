0c52c6fce068821e2668a4ef376e7838db7f07b7
Gibinski <l.gibinski@gmail.com>
2023-12-03 13:03:12 -0800

    Add

---
+++ Section 18 Advanced Python Modules/3_creating_sub_packages.py
@@ -0,0 +1,5 @@
+# ---------------------------------------------------------------------
+# ---------------**## Creating Sub Packages ##**---------------------
+from social_media.videos.user_videos import user_videos
+
+user_videos.video()
