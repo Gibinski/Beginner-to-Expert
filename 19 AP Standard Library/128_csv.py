40b0a38fe8e63a99ce700712c4cb6fede3722c12
Gibinski <l.gibinski@gmail.com>
2023-12-05 08:27:55 -0800

    Remove files

---
+++ 19 AP Standard Library/128_csv.py
@@ -0,0 +1,12 @@
+# ---------------------------------------------------------------------
+# ---------------**## CSV ##**---------------------
+import csv
+
+# ************************* Task One
+# *-*-*--*--*-*** Opening a CSV file and inserting simple data in it.
+
+with open("users_info/users.csv", "w") as users_data:
+    CSV_writer_dara = csv.writer(users_data)
+
+
+
